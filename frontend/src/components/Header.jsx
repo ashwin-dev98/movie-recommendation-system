469ce6590fd4b605504260a53a48b6ad7c72579c
Ashwin M A <u2301058@rajagiri.edu.in>
2026-08-01 23:01:10 +0530

    Create React component structure

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>🎬 Movie Recommendation System</h1>
+      <p>Find movies similar to your favourites.</p>
+    </header>
+  );
+}
+
+export default Header;
